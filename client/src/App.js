0c68ea993e28dff04b038098e2c591b9116f7fef
matt0418 <mfeldma7@gmail.com>
2019-02-22 13:29:49 -0500

    Added some styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,39 @@
 import React, { Component } from 'react';
 import axios from 'axios'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
+const StyledHeader = styled.h1`
+  text-align: center;
+  font-weight: bold;
+  font-size: 45px;
+  background: green;
+  color: white;
+  padding-top: 4%;
+  margin-top: -2%;
+  margin-bottom: 5%;
+  padding-bottom: 3%;
+`
+
+const StyledContainer = styled.div`
+    margin: 0 auto;
+    text-align: center;
+    width: 80%;
+    border: 1px solid black;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+    background: cornsilk;
+    padding: 2% 5%;
+`
+
+const ProjectCard = styled.div`
+    border: 1px solid black;
+    border-radius: 10px;
+    width: 43%;
+    margin: 3%;
+    background: white;
+`
 
 class App extends Component {
   constructor() {
@@ -28,16 +60,20 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        {this.state.projects.map(project => {
-          return(
-            <div>
-              <strong><p>{project.name}</p></strong>
-              <p>{project.description}</p>
-            </div>
-          )
-        })}
+      <div>
+        <StyledHeader>Projects</StyledHeader>
+        <StyledContainer>
+          {this.state.projects.map(project => {
+            return(
+              <ProjectCard>
+                <strong><p>{project.name}</p></strong>
+                <p>{project.description}</p>
+              </ProjectCard>
+            )
+          })}
+        </StyledContainer>
       </div>
+      
     );
   }
 }
